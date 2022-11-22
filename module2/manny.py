58cad5272018e0a6f8d97ee8e8eec6fa74d72a9f
YIN TAI WANG <88786569+YinTaiWang@users.noreply.github.com>
2022-11-22 23:01:22 +0100

    Update manny.py

--- module2/manny.py
+++ module2/manny.py
@@ -1,7 +1,4 @@
 money = 0
-while True:
-    if money < 50:
-        money = float(input("Enter the amount you want to donate: \n"))
-    else:
-        print(f"Thank you very much for your contribution of {format(money, '.2f')} euro.")
-        break
+while money < 50:
+    money = float(input("Enter the amount you want to donate: \n"))
+print(f"Thank you very much for your contribution of {format(money, '.2f')} euro.")
